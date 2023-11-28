03ccfffdcc490a3febb801ab3389e1b6a515eb22
yaonur <ya_onur@hotmail.com>
2023-11-28 23:39:16 +0300

    ss

--- catalin/rustlang/s4-modules/src/main.rs
+++ catalin/rustlang/s4-modules/src/main.rs
@@ -4,5 +4,17 @@ fn main() {
     println!("Hello, world!");
     player::play_movie("The Matrix");
     player::play_audio("The Matrix");
+    clean::clean_house();
+    clean::files::clean_files();
 }
 
+mod clean {
+    pub fn clean_house() {
+        println!("clean house");
+    }
+    pub mod files {
+        pub fn clean_files() {
+            println!("clean files");
+        }
+    }
+}
