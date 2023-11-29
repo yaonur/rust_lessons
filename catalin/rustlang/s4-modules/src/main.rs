8566cc5810a801b0056dcbc9c492771bf8dfaad5
yaonur <ya_onur@hotmail.com>
2023-11-29 03:06:28 +0300

    ss

--- catalin/rustlang/s4-modules/src/main.rs
+++ catalin/rustlang/s4-modules/src/main.rs
@@ -1,4 +1,7 @@
+use crate::archive::arch::arch_file as arc;
+use rand::Rng;
 mod player;
+mod archive;
 
 fn main() {
     println!("Hello, world!");
@@ -6,6 +9,10 @@ fn main() {
     player::play_audio("The Matrix");
     clean::clean_house();
     clean::files::clean_files();
+    arc("The Matrix");
+    let mut rng=rand::thread_rng();
+    let a:i32=rng.gen();
+    println!("a={}",a);
 }
 
 mod clean {
