eee96186ce93492d85df18102a264b0b41cccf69
onur yanık <ya_onur@hotmail.com>
2023-12-26 21:17:19 +0300

    ss

---
+++ letgetrusty/s3-ownership/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // Each value in Rust has a variable that's called its owner.
+    // There can only be one owner at a time.
+    // When the owner goes out of scope, the value will be dropped.
+    {
+        
+    }
+}
