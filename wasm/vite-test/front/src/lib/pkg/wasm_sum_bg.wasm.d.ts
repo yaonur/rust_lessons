38a8811b543ea1927743411fe01c76f258fa60c1
onur yanık <ya_onur@hotmail.com>
2023-12-31 07:46:32 +0300

    ss

---
+++ wasm/vite-test/front/src/lib/pkg/wasm_sum_bg.wasm.d.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function greet(a: number, b: number): void;
+export function __wbindgen_export_0(a: number, b: number): number;
+export function __wbindgen_export_1(a: number, b: number, c: number, d: number): number;
--- wasm/vite-test/wasm-sum/buildWasm.sh
+++ wasm/vite-test/wasm-sum/buildWasm.sh
@@ -1,2 +1,3 @@
 !/bin/bash
-cargo build --release --target wasm32-unknown-unknown
+cargo build --release --target=wasm32-unknown-unknown
+wasm-bindgen target/wasm32-unknown-unknown/release/wasm_sum.wasm --out-dir ../front/src/lib/pkg
